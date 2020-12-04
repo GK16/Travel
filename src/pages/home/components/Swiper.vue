d4db9030e623d17972aa5c004d5b307414fcd4e7
“Guikai <guikai_huang@outlook.com>
2020-12-04 19:29:08 +0800

    add Home swiper

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+        <!-- slides -->
+        <swiper-slide v-for="item of swiperList" :key="item.id">
+            <img class="swiper-img" :src="item.imgUrl">
+        </swiper-slide>
+        <!-- Optional controls -->
+        <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeSwiper',
+    data () {
+        return{
+            swiperOption: {
+                pagination: '.swiper-pagination',
+                loop: true
+            },
+            swiperList: [{
+                id: '0001',
+                imgUrl:''
+            },{
+                id: '0001',
+                imgUrl:''
+            }]
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active //样式进行了穿透
+        background: #fff
+    .wrapper
+        width: 100%
+        height: 0
+        overflow: hidden
+        padding-bottom:31.25% 
+        background: #eee;
+        .swiper-img
+            width: 100%
+
+</style>
