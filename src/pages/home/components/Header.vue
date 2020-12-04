e9c8af5b34ea8deb3989ed108d48d804a6223cbe
“Guikai <guikai_huang@outlook.com>
2020-12-04 16:08:53 +0800

    transfer to stylus

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -20,40 +20,35 @@ export default {
 }
 </script>
 
-<style scoped>
-  /* @import "../../../assets/styles/varibles.styl"; */
-  .header{
-    line-height: .86rem;
-    display: flex;
-    background: #00bcd4;
-    color: #fff;
-  }
-  .header-left{
-      width: .64rem;
-      float: left;
-  }
-  .header-left .back-icon{
-      text-align: center;
-      font-size: .4rem;
-  }
-  .header-input{
-      flex: 1;
-      height: .64rem;
-      line-height: .64rem;
-      margin-top: .12rem;
-      margin-left: .2rem;
-      padding-left: .2rem;
-      background: #fff;
-      border-radius: .1rem;
-      color: #ccc;
-  }
-  .header-right{
-      width: 1.24rem;
-      float: right;
-      text-align: center;
-  }
-  .header-right .arrow-icon{
-      font-size: .24rem;
-      margin-left: -.04rem;
-  }
+<style lang="stylus" scoped>
+  @import "../../../assets/styles/varibles.styl";
+  .header
+    line-height: .86rem
+    display: flex
+    background: $bgColor
+    color: #fff
+    .header-left
+      width: .64rem
+      float: left
+      .back-icon
+        text-align: center
+        font-size: .4rem
+    .header-input
+      flex: 1
+      height: .64rem
+      line-height: .64rem
+      margin-top: .12rem
+      margin-left: .2rem
+      padding-left: .2rem
+      background: #fff
+      border-radius: .1rem
+      color: #ccc
+
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align: center
+      .arrow-icon
+        font-size: .24rem
+        margin-left: -.04rem
 </style>
