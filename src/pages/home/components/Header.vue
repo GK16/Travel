e322d876bd0dec5e39a97e7773da87826c6b5ef0
“Guikai <guikai_huang@outlook.com>
2020-12-05 21:30:04 +0800

    city-router page init

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -6,11 +6,13 @@
         <div class="header-input">
             <span class="iconfont">&#xe632;</span>
             输入城市/景点/游玩主题
-            </div>
-        <div class="header-right">
-            {{this.city}}
-            <span class="iconfont arrow-icon">&#xe64a;</span>
-            </div>
+        </div>
+        <router-link to='/city'>
+          <div class="header-right">
+              {{this.city}}
+              <span class="iconfont arrow-icon">&#xe64a;</span>
+          </div>
+        </router-link>
     </div>
 </template>
 
@@ -26,7 +28,7 @@ export default {
 <style lang="stylus" scoped>
   @import "../../../assets/styles/varibles.styl";
   .header
-    line-height: .86rem
+    line-height: $headerHeight
     display: flex
     background: $bgColor
     color: #fff
@@ -51,6 +53,7 @@ export default {
       width: 1.24rem
       float: right
       text-align: center
+      color #fff
       .arrow-icon
         font-size: .24rem
         margin-left: -.04rem
