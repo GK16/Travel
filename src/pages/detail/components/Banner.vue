e43063170ae1bca6559e37bc12a71bde3662dcc2
“Guikai <guikai_huang@outlook.com>
2020-12-10 15:27:38 +0800

    banner finished

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,77 @@
+<template>
+    <div>
+        <div class="banner" @click="handleBannerClick">
+            <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_600x330_f922b488.jpg" alt="">
+            <div class="banner-info">
+                <div class="banner-title">故宫(AAAAA景区)</div>
+                <div class="banner-number">
+                    <span class="iconfont banner-icon">&#xe6c8;</span>
+                    39
+                </div>
+            </div>
+        </div>
+        <common-gallary 
+            :imgs="imgs" 
+            v-show="showGallary"
+            @close='handleGallaryClose'
+        >
+        </common-gallary>
+    </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+    name: 'DetailBanner',
+    data () {
+        return{
+            showGallary: false,
+            imgs: ['http://img1.qunarzz.com/sight/p0/1909/4e/4e1afc2e1133ade7a3.img.jpg_r_800x800_d3eb324d.jpg','http://img1.qunarzz.com/sight/p0/1410/e3/73da8d3e19cdc41c1932d4fcd22ec792.water.jpg_r_800x800_7f96ccf9.jpg']
+        }
+    },
+    methods: {
+        handleBannerClick(){
+            this.showGallary = true
+        },
+        handleGallaryClose(){
+            this.showGallary = false
+        }
+    },
+    components:{
+        CommonGallary
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .banner
+        position relative
+        overflow hidden
+        height 0
+        padding-bottom 55%
+        .banner-img
+            width 100%
+        .banner-info
+            color #fff
+            display flex
+            position absolute
+            left 0
+            right 0
+            bottom 0
+            line-height .6rem
+            background-image linear-gradient(top, rgba(0,0,0,0),rgba(0,0,0,0.8))
+            .banner-title
+                flex 1
+                font-size .32rem
+                padding 0 .2rem
+            .banner-number
+                padding 0 .4rem
+                line-height .32rem
+                height .32rem
+                margin-top .14rem
+                border-radius .2rem
+                background rgba(0,0,0, .8)
+                font-size .24rem
+                .banner-icon
+                    font-size .24rem
+</style>
